cc8ad0565216dbd43887a31c54147fd29818c904
Idan Entin <idanen@gmail.com>
2023-02-02 14:45:22 +0200

    ci: fix base property

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'excercise-generator',
+  base: '/excercise-generator',
   plugins: [react()],
   test: {
     globals: true,
